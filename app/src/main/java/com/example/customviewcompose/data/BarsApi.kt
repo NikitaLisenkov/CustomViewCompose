ba12db97291fc4e27e5ef2e4eff75af7f817b8e3
Nikita <predatorrr@mail.ru>
2025-07-17 17:43:33 +0500

    Add scroll and scaling to terminal

--- app/src/main/java/com/example/customviewcompose/data/BarsApi.kt
+++ app/src/main/java/com/example/customviewcompose/data/BarsApi.kt
@@ -4,6 +4,6 @@ import retrofit2.http.GET
 
 interface BarsApi {
 
-    @GET("aggs/ticker/AAPL/range/1/hour/2024-06-09/2025-06-10?adjusted=true&sort=asc&limit=50000&apiKey=c7usvog5w2Bz6PpIbgr1pPA9eQE8encE")
+    @GET("aggs/ticker/AAPL/range/1/hour/2024-06-09/2025-06-10?adjusted=true&sort=desc&limit=50000&apiKey=c7usvog5w2Bz6PpIbgr1pPA9eQE8encE")
     suspend fun getBars(): Result
 }
--- app/src/main/java/com/example/customviewcompose/presentation/BarsScreen.kt
+++ app/src/main/java/com/example/customviewcompose/presentation/BarsScreen.kt
@@ -2,14 +2,26 @@ package com.example.customviewcompose.presentation
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.TransformableState
+import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.translate
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.customviewcompose.data.Bar
+import kotlin.math.roundToInt
+
+
+private const val MIN_VISIBLE_BARS_COUNT = 20
 
 @Composable
 fun BarsScreen() {
@@ -32,6 +44,44 @@ fun BarsScreen() {
 
 @Composable
 fun Terminal(bars: List<Bar>) {
+
+    var scrolledBy by remember {
+        mutableStateOf(0f)
+    }
+
+    var visibleBarsCount by remember {
+        mutableStateOf(100)
+    }
+
+    var terminalWidth by remember {
+        mutableStateOf(0f)
+    }
+
+    val barWidth by remember {
+        derivedStateOf {
+            terminalWidth / visibleBarsCount
+        }
+    }
+
+    val visibleBars by remember {
+        derivedStateOf {
+            val startIndex = (scrolledBy / barWidth).roundToInt().coerceAtLeast(0)
+            val endIndex = (startIndex + visibleBarsCount).coerceAtMost(bars.size)
+            bars.subList(fromIndex = startIndex, toIndex = endIndex)
+        }
+    }
+
+
+    val transformableState = TransformableState { zoomChange, panChange, _ ->
+        visibleBarsCount = (visibleBarsCount / zoomChange)
+            .roundToInt()
+            .coerceIn(MIN_VISIBLE_BARS_COUNT, bars.size)
+
+        scrolledBy = (scrolledBy + panChange.x)
+            .coerceAtLeast(0f)
+            .coerceAtMost(bars.size * barWidth - terminalWidth)
+
+    }
     /**
      * Нам нужно каждую свечку распологать справа от предыдущей, свеча с индексом 0 будет раположена в левой части экрана,
      * свеча с индексом 1 правее и т.д. Для рассчета отступа мы взяли ширину экрана и разделили на кол-во свечей. После этого
@@ -46,19 +96,29 @@ fun Terminal(bars: List<Bar>) {
         modifier = Modifier
             .fillMaxSize()
             .background(Color.Black)
+            .transformable(transformableState)
     ) {
-        val maxPrice = bars.maxOf { it.high }
-        val minPrice = bars.minOf { it.low }
-        val barWidth = size.width / bars.size
+        terminalWidth = size.width
+        val maxPrice = visibleBars.maxOf { it.high }
+        val minPrice = visibleBars.minOf { it.low }
         val pxPerPoint = size.height / (maxPrice - minPrice)
-        bars.forEachIndexed() { index, bar ->
-            val offsetX = index * barWidth
-            drawLine(
-                color = Color.White,
-                start = Offset(offsetX, size.height - ((bar.low - minPrice) * pxPerPoint)),
-                end = Offset(offsetX, size.height - ((bar.high - minPrice) * pxPerPoint)),
-                strokeWidth = 1f
-            )
+
+        translate(left = scrolledBy) {
+            bars.forEachIndexed { index, bar ->
+                val offsetX = size.width - index * barWidth
+                drawLine(
+                    color = Color.White,
+                    start = Offset(offsetX, size.height - ((bar.low - minPrice) * pxPerPoint)),
+                    end = Offset(offsetX, size.height - ((bar.high - minPrice) * pxPerPoint)),
+                    strokeWidth = 1f
+                )
+                drawLine(
+                    color = if (bar.open < bar.close) Color.Green else Color.Red,
+                    start = Offset(offsetX, size.height - ((bar.open - minPrice) * pxPerPoint)),
+                    end = Offset(offsetX, size.height - ((bar.close - minPrice) * pxPerPoint)),
+                    strokeWidth = barWidth / 2
+                )
+            }
         }
     }
 }
